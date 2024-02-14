d0b12742554f2d3991c77a68e699c1628e74d213
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-02-14 17:43:27 +0200

    done-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,52 +5,6 @@ import { Filter } from './Filter';
 import { ContactList } from './ContactList';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState(
-  //   () => JSON.parse(localStorage.getItem('contacts')) || []
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const handleSubmit = (e, name, number) => {
-  //   e.preventDefault();
-
-  //   const isExist = contacts
-  //     .map(({ name }) => name.toLowerCase())
-  //     .includes(name.toLowerCase());
-
-  //   if (isExist) {
-  //     alert(`${name} is already in contacts.`);
-  //     return;
-  //   }
-
-  //   setContacts(prevContacts => [
-  //     ...prevContacts,
-  //     { id: `${nanoid()}`, name: `${name}`, number: `${number}` },
-  //   ]);
-  // };
-
-  // const handleChange = e => {
-  //   setFilter(e.target.value);
-  // };
-
-  // const filterContacts = () => {
-  //   if (filter === '') {
-  //     return contacts;
-  //   }
-  //   const filtered = contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(filter.toLowerCase())
-  //   );
-  //   return filtered;
-  // };
-
-  // const handleClick = e => {
-  //   const updatedContacts = contacts.filter(({ id }) => id !== e.target.id);
-  //   setContacts([...updatedContacts]);
-  // };
-
   return (
     <Div>
       <Heading>Phonebook</Heading>
